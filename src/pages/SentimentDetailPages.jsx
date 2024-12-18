01189472ec4397231d57b4d2a0f90abe4bcc295d
wahyutc <hawkxc068@gmail.com>
2024-12-18 12:56:39 +0700

    sentiment detail

--- src/pages/SentimentDetailPages.jsx
+++ src/pages/SentimentDetailPages.jsx
@@ -1,4 +1,17 @@
 const SentimentDetailPages = ({ detailSentiment, handleBackToSentiments }) => {
+  const reaction = {
+    positive: detailSentiment.statistic.data.positive,
+    neutral: detailSentiment.statistic.data.neutral,
+    negative: detailSentiment.statistic.data.negative,
+  };
+
+  // const totalReaction = reaction.reduce((a, b) => a + b, 0);
+  const reactionOverall = Object.keys(reaction).find(
+    (key) => reaction[key] === Math.max(...Object.values(reaction))
+  );
+
+  console.log(reactionOverall);
+
   console.log(detailSentiment);
   return (
     <div className="p-5 bg-white rounded-lg shadow-lg">
@@ -6,11 +19,51 @@ const SentimentDetailPages = ({ detailSentiment, handleBackToSentiments }) => {
         onClick={handleBackToSentiments}
         className="mb-3 bg-gray-200 btn hover:bg-gray-300"
       >
-        Back to Sentiments
+        <i class="ri-arrow-left-s-line"></i> Back to Sentiments
       </button>
-      <h1 className="items-center justify-center text-3xl font-bold text-center">
-        〰️{detailSentiment.title || detailSentiment.unique_id}〰️
-      </h1>
+      <div className="flex flex-col border rounded-lg shadow-sm border-slate-200">
+        <div className="flex flex-col p-5">
+          <h1 className="text-2xl">
+            {detailSentiment.title || `Sentiment ${detailSentiment.unique_id}`}
+          </h1>
+          <span className="flex flex-col">
+            {detailSentiment.sentiment_link.split(", ").map((item, index) => (
+              <a
+                href={item}
+                target="_blank"
+                key={index}
+                className="text-blue-300 hover:text-blue-500"
+              >
+                <i class="ri-links-line"></i> {detailSentiment.platform} link{" "}
+                {index + 1}
+              </a>
+            ))}
+          </span>
+        </div>
+        <hr className="w-full border" />
+        <div className="flex flex-row justify-between p-5">
+          <div>
+            <div className="flex flex-col gap-2">
+              <span className="font-semibold text-blue-400">
+                <i class="ri-information-line"></i> Status
+              </span>
+              <span className="flex flex-row items-center justify-center gap-2 p-4 align-middle badge border-slate-200">
+                <span className="badge badge-xs badge-success"></span>
+                Success
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="font-semibold text-blue-400">
+                <i class="ri-time-line"></i> Reaction
+              </span>
+              <span className="flex flex-row items-center justify-center gap-2 p-4 align-middle badge border-slate-200">
+                <span className="badge badge-xs badge-success"></span>
+                {reactionOverall}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div></div>
       <p>Platform: {detailSentiment.platform}</p>
       <p>Created At: {detailSentiment.created_at}</p>
